8ec62767e29e94cab5e957dccb8ee6eb72f031fd
Bruno <brunoariel.dev@gmail.com>
2025-02-06 02:27:28 -0300

    correcionHome1

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -16,24 +16,22 @@ const HomeScreen = () => {
     return (
         <main className='home-screen'>
             <h1>Bienvenido de vuelta {/* nombre del usuario */} </h1>
-            
+
             <div className='workspaces-container'>
                 {/* Sección de Workspaces */}
                 <div className='workspaces'>
                     {
                         workspace_loading ? (
                             <h2>Cargando...</h2>
+                        ) : workspace_response?.data?.workspaces?.length > 0 ? (
+                            workspace_response.data.workspaces.map(workspace => (
+                                <div key={workspace._id} className="workspace-item">
+                                    <h3>{workspace.name}</h3>
+                                    <Link to={`/workspace/${workspace._id}`}>Ir a workspace</Link>
+                                </div>
+                            ))
                         ) : (
-                            workspace_response.data.workspaces.length > 0? (
-                                workspace_response.data.workspaces.map(workspace => (
-                                    <div key={workspace._id} className="workspace-item">
-                                        <h3>{workspace.name}</h3>
-                                        <Link to={`/workspace/${workspace._id}`}>Ir a workspace</Link>
-                                    </div>
-                                ))
-                            ) : (
-                                <h2>No tienes espacios de trabajo</h2>
-                            )
+                            <h2>No tienes espacios de trabajo</h2>
                         )
                     }
                 </div>
