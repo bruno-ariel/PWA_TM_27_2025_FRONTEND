3978f9967466716afa1935b2dc9b776caef385fa
Bruno <brunoariel.dev@gmail.com>
2025-02-09 11:33:34 -0300

    createChannel2

--- src/screens/CreateChannelScreen.jsx
+++ src/screens/CreateChannelScreen.jsx
@@ -19,42 +19,21 @@ const CreateChannelScreen = () => {
         navigate('/workspace')
     }
     return (
-        <div>
+    <div>
         <h1>Crear un canal</h1>
         <form onSubmit={handleCreateChannel}>
             <div>
-                <label htmlFor="name">Nombre del canal</label>
-                <input
-                    id="name"
-                    type="text"
-                    name="name"
-                    placeholder="nombre del canal"
-                    onChange={handleChangeInput}
-                    value={form_state.name}
+                <label htmlFor='name'></label>
+                <input 
+                id='name' 
+                type='text' 
+                name='name' 
+                placeholder='nombre del canal' 
+                onChange={handleChangeInput}
+                value={form_state.name}
                 />
             </div>
-            <div>
-                <label htmlFor="description">Descripción del canal (opcional)</label>
-                <input
-                    id="description"
-                    type="text"
-                    name="description"
-                    placeholder="añadir una descripción"
-                    onChange={handleChangeInput}
-                    value={form_state.description}
-                />
-            </div>
-            <div>
-                <input
-                    id="isPrivate"
-                    type="checkbox"
-                    name="isPrivate"
-                    onChange={handleChangeInput}
-                    checked={form_state.isPrivate}
-                />
-                <label htmlFor="isPrivate">Hacer privado</label>
-            </div>
-            <button type="submit">Crear canal</button>
+            <button type='submit'> Crear canal </button>
         </form>
     </div>
     )
