5f9089fdb8efc4f130e3ce93de6f27550e674dea
Bruno <brunoariel.dev@gmail.com>
2025-02-10 05:37:49 -0300

    createChannel4

--- src/screens/WorkspaceScreen.jsx
+++ src/screens/WorkspaceScreen.jsx
@@ -28,7 +28,7 @@ const WorkspaceScreen = () => {
                         workspace_id={workspace_id} 
                     />
                 )}
-                <Link className="add-channel" to={`/channel/new`}>
+                <Link className="add-channel" to={`/workspace/${workspace_id}/create-channel`}>
                     + Añadir Canal
                 </Link>
             </aside>
