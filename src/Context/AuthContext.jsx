d583f2881f24ffbdeaad28daf03cbfc55ba46f0e
Bruno <brunoariel.dev@gmail.com>
2025-02-19 01:02:03 -0300

    FrontendFinal

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,28 +1,26 @@
-import { createContext, useEffect, useState } from "react"
+import { useState } from "react"
+import { useEffect } from "react"
+import { use } from "react"
+import { createContext } from "react"
 
-//Crear el contexto
 export const AuthContext = createContext()
 
+const AuthContextProvider = ({children}) => {
+    let isAuthenticated = Boolean(sessionStorage.getItem("access_token"))
+    const [isAuthenticatedState, setIsAuthenticatedState] = useState(isAuthenticated)
 
-//Crear el proveedor de contexto (Es un componente)dd
-
-export const AuthContextProvider = ({children}) =>{
-
-    const [isAuthenticatedState, setIsAuthenticatedState] = useState(Boolean(sessionStorage.getItem('access_token')))
-    const login = (access_token) =>{
-        sessionStorage.setItem('access_token', access_token)
-        setIsAuthenticatedState(true)
-    }
-    /*  useEffect(() =>{
-        const auth_token = sessionStorage.getItem('access_token')
-        if(auth_token) {
+    /* useEffect ( () => {
+        const auth_token = sessionStorage.getItem("access_token")
+        if (auth_token) {
             setIsAuthenticatedState(true)
         }
-    }, []) */
+    }, [] ) */
 
     return (
-        <AuthContext.Provider value={{isAuthenticatedState, login}}>
+        <AuthContext.Provider value={{isAuthenticatedState}}>
             {children}
         </AuthContext.Provider>
     )
 }
+
+export default AuthContextProvider
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,44 +1,36 @@
-import React, { useState, useContext } from 'react'
+import React from 'react'
 import useForm from '../hooks/useForm'
-import ENVIROMENT from '../utils/constants/enviroment'
-import { Link, useNavigate} from 'react-router-dom'
-import { AuthContext } from '../Context/AuthContext'
+import ENVIROMENT from '../utils/constants/enviroments'
+import { Link , useNavigate } from 'react-router-dom'
 const LoginScreen = () => {
-
-    const {login, isAuthenticatedState} = useContext(AuthContext)
-    console.log('Authenticated:', isAuthenticatedState)
     const navigate = useNavigate()
-    const {form_state, handleChangeInput} = useForm({email:'', password: ''})
-    const url = new URLSearchParams(window.location.search)
+    const { form_state, handleChangeInput } = useForm ({ email: "", password: "" })
+    const url = new URLSearchParams(window.location.search) 
     if(url.get('verified')){
-        alert('Cuenta verificada')
+        alert('tu cuenta ha sido verificada')
     }
-    const handleSubmitForm = async (event) =>{
-        
+    const handleSubmitForm = async (e) => {
         try{
-            event.preventDefault()
-            const response = await fetch(ENVIROMENT.API_URL + '/api/auth/login', {
-                method: "POST",
-                headers:{
-                    'Content-Type': "application/json"
-                },
+            e.preventDefault()
+            const response = await fetch( ENVIROMENT.API_URL + '/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+            },
                 body: JSON.stringify(form_state)
-            })
+        })
             const data = await response.json()
-
-            login(data.data.access_token)
-            navigate('/home')
+            console.log(data)
+            sessionStorage.setItem('access_token', data.data.access_token)
+            navigate("/home")
         }
         catch(error){
-            console.error("Error al loguear", error)
+            console.error(' error al loguear ', error)
         }
-        
     }
     const errores = {
-        email: [
-        ],
-        password: [
-        ]
+        email: [],
+        password: []
     }
     form_state.email && form_state.email.length > 30 && errores.email.push('El email no puede superar los 30 caracteres')
     form_state.email && form_state.email.length < 5 && errores.email.push('El email debe tener al menos 5 caracteres')
