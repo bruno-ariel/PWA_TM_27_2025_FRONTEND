9fe1198c9fa08c17a636a9407fb9a0b1d68d04f3
Bruno <brunoariel.dev@gmail.com>
2025-02-19 00:33:41 -0300

    ultimaprueba1

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -3,31 +3,38 @@ import useForm from '../hooks/useForm'
 import ENVIROMENT from '../utils/constants/enviroments'
 import { Link , useNavigate } from 'react-router-dom'
 const LoginScreen = () => {
-    const navigate = useNavigate()
     const { form_state, handleChangeInput } = useForm ({ email: "", password: "" })
-    const url = new URLSearchParams(window.location.search) 
+    const url = new URLSearchParams(window.location.search)
+    const navigate = useNavigate()
     if(url.get('verified')){
         alert('tu cuenta ha sido verificada')
     }
     const handleSubmitForm = async (e) => {
-        try{
-            e.preventDefault()
-            const response = await fetch(`${ENVIROMENT.API_URL}/api/auth/login`, {
+        try {
+            e.preventDefault();
+            const response = await fetch(ENVIROMENT.API_URL + '/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
-            },
+                },
                 body: JSON.stringify(form_state)
-        })
-            const data = await response.json()
-            console.log(data)
-            sessionStorage.setItem('access_token', data.data.access_token)
-            navigate("/home")
+            });
+    
+            const data = await response.json();
+            console.log("Respuesta del backend:", data);
+    
+            if (!response.ok) {
+                console.error("Error en el login:", data);
+                return;
+            }
+    
+            sessionStorage.setItem('access_token', data.data.access_token);
+            navigate("/home"); // Esto se ejecutará solo si la respuesta es exitosa
+        } catch (error) {
+            console.error('Error al loguear:', error);
         }
-        catch(error){
-            console.error(' error al loguear ', error)
-        }
-    }
+    };
+    
     const errores = {
         email: [],
         password: []
